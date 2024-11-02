c1a3d4aa27ef43dc193ba723bd9d68793b1d3574
SUNKARA VENKATA NAVEEN <40927874+venkat3697@users.noreply.github.com>
2024-11-02 18:34:46 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL: "https://taskmanagerserver-5c0t.onrender.com/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
